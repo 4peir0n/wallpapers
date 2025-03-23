15bf7df927c5ef080b564c07c5f4d8698ded5445
Daniel <159633432+4peir0n@users.noreply.github.com>
2025-03-23 18:23:27 -0500

    Update images.js

--- images.js
+++ images.js
@@ -3,9 +3,9 @@ const images = [
     "2.webp",
     "fondo3.png",
     "zoro.jpg",
-    "image4.jpg"
-    "edge-runner.jpg"
-    "wallpaper.png"
-    "purple.png"
+    "image4.jpg",
+    "edge-runner.jpg",
+    "wallpaper.png",
+    "purple.png",
     "rebecca.png"
 ];
